5fea25e9747ba40ca71545c22e58865fe8b29c66
shubhamkumbhakar <shubhamkumbhakar27@gmail.com>
2020-08-11 10:03:32 +0530

    Updates

---
+++ precache-manifest.2cdee81b3ca32dacc57399e33ffe2b5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbf781c0d9c426001d4c0b01656ebb3",
+    "url": "/Techolution-assignment/index.html"
+  },
+  {
+    "revision": "d7c0e4f306998f6bf708",
+    "url": "/Techolution-assignment/static/css/main.68d73ebf.chunk.css"
+  },
+  {
+    "revision": "d294eb661b03336915a6",
+    "url": "/Techolution-assignment/static/js/2.fd1ba824.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Techolution-assignment/static/js/2.fd1ba824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7c0e4f306998f6bf708",
+    "url": "/Techolution-assignment/static/js/main.5c9fff51.chunk.js"
+  },
+  {
+    "revision": "4ecd7a7a4c6b423b8ef2",
+    "url": "/Techolution-assignment/static/js/runtime-main.041dfb7e.js"
+  }
+]);
